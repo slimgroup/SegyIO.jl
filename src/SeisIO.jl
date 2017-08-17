70dbdaecbb0d5f3916b25c1d4cdcf6a89897c123
Keegan Lensink <keeganlensink@gmail.com>
2017-08-17 16:02:43 -0700

    organize

--- src/SeisIO.jl
+++ src/SeisIO.jl
@@ -4,12 +4,12 @@ module SeisIO
     include("BinaryFileHeader.jl")
     include("BinaryTraceHeader.jl")
     include("IBMFloat32.jl")
-    include("SeisBlock.jl")
+    include("read/SeisBlock.jl")
 
     #Functions
-    include("read_fileheader.jl")
-    include("read_traceheader.jl")
-    include("read_trace.jl")
-    include("read_file.jl")
+    include("read/read_fileheader.jl")
+    include("read/read_traceheader.jl")
+    include("read/read_trace.jl")
+    include("read/read_file.jl")
      
 end # module
