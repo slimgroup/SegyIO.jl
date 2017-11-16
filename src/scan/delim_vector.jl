03db13e3a5a092de8135f48e473253104fe05e2a
Keegan Lensink <keeganlensink@gmail.com>
2017-11-16 11:50:47 -0800

    document delim_vector

--- src/scan/delim_vector.jl
+++ src/scan/delim_vector.jl
@@ -1,5 +1,41 @@
 export delim_vector
 
+"""
+    Use: delim_vector(x::AbstractVector, probe_length::Int)
+
+Return the indicies of the first element for each homogenous section in `x`.
+
+# Example
+```
+julia> x = vec([1 1 1 2 2 2 2 3 3 3 3]);
+
+julia> delim_vector(x, 1)
+3-element Array{Int64,1}:
+1
+4
+8
+```
+
+`probe_length` sets the intial guess for the section length. Choosing `probe_length` close the the true length of the section provides modest performance benefits, however simply setting the length to 1 is not a huge hit.
+
+```
+julia> @btime delim_vector(x,1)
+  1.169 μs (3 allocations: 176 bytes)
+4-element Array{Int64,1}:
+      1
+ 100001
+ 100003
+ 200003
+
+julia> @btime delim_vector(x,100000)
+  945.615 ns (3 allocations: 176 bytes)
+4-element Array{Int64,1}:
+      1
+ 100001
+ 100003
+ 200003
+```
+"""
 function delim_vector(x::AbstractVector, probe_length::Int)
 
     delims = Array{Int,1}(1)
--- src/scan/find_next_delim.jl
+++ src/scan/find_next_delim.jl
@@ -1,5 +1,21 @@
 export find_next_delim
 
+"""
+    Use: find_next_delim(x::AbstractVector, i::Int, probe_length::Int)
+
+From starting index `i`, return the index of the next element in the vector that holds a different value.
+
+# EXAMPLE
+```
+julia> x = vec([1 1 1 2 2 2 2 3 3 3 3]);
+
+julia> find_next_delim(x, 1, 1)
+4
+
+julia> find_next_delim(x, 4, 1)
+8
+```
+"""
 function find_next_delim(x::AbstractVector, i::Int, probe_length::Int)
     
     # Setup
