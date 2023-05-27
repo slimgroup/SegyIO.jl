f84e2a1a5b04e3a96008f59cf5fa5fa6d7acbfb6
Kerim Khemraev <kerim.khemraev@mail.ru>
2023-05-28 01:48:37 +0300

    Added SeisCon copy function
    This function is required by JUDI's judiVector:
    https://github.com/slimgroup/JUDI.jl/blob/16aa54abc9e1a18d8bd59636895afbd90718b0b7/src/TimeModeling/Types/judiVector.jl#L156

--- src/types/SeisCon.jl
+++ src/types/SeisCon.jl
@@ -1,7 +1,7 @@
 # SeisCon type definition and methods
 import Base.size, Base.length, Base.getindex, Base.show
 
-export SeisCon, size, getindex, merge_con, split_con, show
+export SeisCon, size, getindex, copy, merge_con, split_con, show
 
 struct SeisCon
     ns::Int
@@ -19,6 +19,13 @@ end
 function getindex(con::SeisCon, a::Colon) 
     read_con(con, 1:length(con))
 end
+
+"""
+    copy(s::SeisCon)
+
+Create a copy of SeisCon object.
+"""
+copy(s::SeisCon) = SeisCon(s.ns, s.dsf, s.blocks)
 
 #=
 function show(s::SeisCon)
